18722c697e6ef11115cc66b53ae6bd38d7aa9caa
Jason Bailey <jmbailey816@gmail.com>
2021-05-07 21:20:32 -0400

    Updated js file

--- assets/script.js
+++ assets/script.js
@@ -13,10 +13,10 @@ window.onscroll = function() {
 
 
 // modal images start
-// Get the modal
+// get the modal
 var modal = document.getElementById("myModal");
 
-// Get the image and insert it inside the modal - use its "alt" text as a caption
+// get the image and insert it inside the modal - use its "alt" text as a caption
 var img = document.getElementById("myImg");
 var modalImg = document.getElementById("img01");
 var captionText = document.getElementById("caption");
@@ -26,10 +26,10 @@ img.onclick = function(){
   captionText.innerHTML = this.alt;
 }
 
-// Get the <span> element that closes the modal
+// get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks on <span> (x), close the modal
+// when the user clicks on <span> (x), close the modal
 span.onclick = function() { 
   modal.style.display = "none";
 }
